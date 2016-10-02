de2a98703435ad0e4a2f352d1cb74e88f18b25f6
Marcus Vinícius <Marcus Vincius>
2016-10-02 20:39:40 -0300

    First commit.

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log (name);
+	}
+
+	public void QuitRequest() {
+		Debug.Log ("Quit");
+	}
+
+}
